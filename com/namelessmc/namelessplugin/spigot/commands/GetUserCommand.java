904c9cc4dc1130537f74fe4ffa85494d859a94a0
Sam <timmy437@hotmail.co.uk>
2016-12-06 20:11:49 +0000

    Fix issues, also add registration date
    Fix a couple of issues, also adds registration date to /getuser command

--- com/namelessmc/namelessplugin/spigot/commands/GetUserCommand.java
+++ com/namelessmc/namelessplugin/spigot/commands/GetUserCommand.java
@@ -108,13 +108,16 @@ public class GetUserCommand implements CommandExecutor {
 							sender.sendMessage(ChatColor.RED + "Error: " + response.get("message").toString());
 						} else {
 							
+							// Convert UNIX timestamp to date
+							java.util.Date registered = new java.util.Date(Long.parseLong(message.get("registered").toString()) * 1000);
+							
 							// Display get user.
-							sender.sendMessage("ยง3ยงm--------------------------------");
+							sender.sendMessage("ง3งm--------------------------------");
 							sender.sendMessage(ChatColor.GREEN + "Username: " + ChatColor.AQUA + message.get("username").toString());
 							sender.sendMessage(ChatColor.GREEN + "DisplayName: " + ChatColor.AQUA + message.get("displayname").toString());
 							sender.sendMessage(ChatColor.GREEN + "UUID: " + ChatColor.AQUA + message.get("uuid").toString());
 							sender.sendMessage(ChatColor.GREEN + "Group ID: " + ChatColor.AQUA + message.get("group_id").toString());
-							//sender.sendMessage(ChatColor.GREEN + "Registered: " + ChatColor.AQUA + message.get("registered").toString());
+							sender.sendMessage(ChatColor.GREEN + "Registered: " + ChatColor.AQUA + registered);
 							sender.sendMessage(ChatColor.GREEN + "Reputation: " + ChatColor.AQUA + message.get("reputation").toString());
 							
 							// check if validated
@@ -129,7 +132,7 @@ public class GetUserCommand implements CommandExecutor {
 			                } else{
 			                	sender.sendMessage(ChatColor.RED + "Banned: " + ChatColor.GREEN + "No!");
 			                }
-							sender.sendMessage("ยง3ยงm--------------------------------");
+							sender.sendMessage("ง3งm--------------------------------");
 						}
 						
 						// Close output/input stream
