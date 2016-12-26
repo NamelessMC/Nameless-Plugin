5dd21f18e0f53ce538a045cf26da74da977a6129
IsS127 <saker.web1@gmail.com>
2016-12-26 17:27:31 +0100

    Added UpdateUserName & getUserName to [Spigot]
    Added UpdateUserName & getUserName to RequstUtil in Spigot

--- nameless-spigot/src/main/java/com/namelessmc/namelessplugin/spigot/utils/RequestUtil.java
+++ nameless-spigot/src/main/java/com/namelessmc/namelessplugin/spigot/utils/RequestUtil.java
@@ -121,6 +121,105 @@ public class RequestUtil {
 			return message.get("group_id").toString();
 		}
 	}
+	
+	public String getUserName(String playerName) throws Exception{
+		String toPostString = "username=" + URLEncoder.encode(playerName, "UTF-8");
+
+		URL apiConnection = new URL(plugin.getAPIUrl() + "/get");
+
+		HttpURLConnection connection = (HttpURLConnection) apiConnection.openConnection();
+		connection.setRequestMethod("POST");
+		connection.setRequestProperty("Content-Length", Integer.toString(toPostString.length()));
+		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		connection.setDoOutput(true);
+		connection.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+
+		// Initialise output stream
+		DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+
+		// Write request
+		outputStream.writeBytes(toPostString);
+
+        InputStream inputStream = connection.getInputStream();
+		
+		// Handle response
+		BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+		StringBuilder responseBuilder = new StringBuilder();
+		
+		String responseString;
+		while((responseString = streamReader.readLine()) != null)
+			responseBuilder.append(responseString);
+		JSONParser parser = new JSONParser();
+		JSONObject response = new JSONObject();
+		
+		response = (JSONObject) parser.parse(responseBuilder.toString());
+		JSONObject message = (JSONObject) parser.parse(response.get("message").toString());
+		
+		// Close output/input stream
+		outputStream.flush();
+		outputStream.close();
+		inputStream.close();
+
+		// Disconnect
+		connection.disconnect();
+
+		if(response.containsKey("error")){
+			plugin.getLogger().info(ChatColor.RED + "Error: " + response.get("message").toString());
+			return null;
+		} else {
+			return message.get("username").toString();
+		}
+	}
+	
+	public void updateUserName(String uuid, String newUsername) throws Exception{
+		String toPostString = "id=" + URLEncoder.encode(uuid, "UTF-8") 
+			+ "&new_username=" + URLEncoder.encode(newUsername, "UTF-8");
+
+		URL apiConnection = new URL(plugin.getAPIUrl() + "/setGroup");
+
+		HttpURLConnection connection = (HttpURLConnection) apiConnection.openConnection();
+		connection.setRequestMethod("POST");
+		connection.setRequestProperty("Content-Length", Integer.toString(toPostString.length()));
+		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		connection.setDoOutput(true);
+		connection.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+
+		// Initialise output stream
+		DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+
+		// Write request
+		outputStream.writeBytes(toPostString);
+
+		// Initialise input stream
+		InputStream inputStream = connection.getInputStream();
+
+		// Handle response
+		BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+		StringBuilder responseBuilder = new StringBuilder();
+
+		String responseString;
+		while((responseString = streamReader.readLine()) != null)
+			responseBuilder.append(responseString);
+		JSONObject response = new JSONObject();
+		JSONParser parser = new JSONParser();
+
+		response = (JSONObject) parser.parse(responseBuilder.toString());
+
+		if(response.containsKey("error")){
+			// Error with request
+			Bukkit.getLogger().info(ChatColor.RED + "Error: " + response.get("message").toString());
+		} else {
+			Bukkit.getLogger().info(ChatColor.GREEN + "Succesfully changed " + uuid + "'s username to " + newUsername);
+		}
+
+		// Close output/input stream
+		outputStream.flush();
+		outputStream.close();
+		inputStream.close();
+
+		// Disconnect
+		connection.disconnect();
+	}
 
 	public void getNotifications(UUID uuid) throws Exception{
 		MessagesUtil messages = new MessagesUtil(plugin);
