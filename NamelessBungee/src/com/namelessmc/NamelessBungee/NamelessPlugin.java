d727bb4baba491cacef245b1c9bb2be1770b23ec
RobinMC <robinslot@hotmail.nl>
2017-07-21 21:46:37 +0200

    Update main class

--- NamelessBungee/src/com/namelessmc/NamelessBungee/NamelessPlugin.java
+++ NamelessBungee/src/com/namelessmc/NamelessBungee/NamelessPlugin.java
@@ -1,25 +1,27 @@
 package com.namelessmc.NamelessBungee;
 
-import com.namelessmc.namelessplugin.bungeecord.API.NamelessAPI;
-import com.namelessmc.namelessplugin.bungeecord.API.UpdateChecker;
-import com.namelessmc.namelessplugin.bungeecord.API.Utils.NamelessChat;
-import com.namelessmc.namelessplugin.bungeecord.API.Utils.NamelessMessages;
-import com.namelessmc.namelessplugin.bungeecord.commands.CommandWithArgs;
-import com.namelessmc.namelessplugin.bungeecord.commands.alone.GetNotificationsCommand;
-import com.namelessmc.namelessplugin.bungeecord.commands.alone.GetUserCommand;
-import com.namelessmc.namelessplugin.bungeecord.commands.alone.RegisterCommand;
-import com.namelessmc.namelessplugin.bungeecord.commands.alone.ReportCommand;
-import com.namelessmc.namelessplugin.bungeecord.commands.alone.SetGroupCommand;
-import com.namelessmc.namelessplugin.bungeecord.player.PlayerEventListener;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+
+import com.namelessmc.NamelessAPI.NamelessAPI;
+import com.namelessmc.NamelessBungee.commands.CommandWithArgs;
+import com.namelessmc.NamelessBungee.commands.GetNotificationsCommand;
+import com.namelessmc.NamelessBungee.commands.GetUserCommand;
+import com.namelessmc.NamelessBungee.commands.RegisterCommand;
+import com.namelessmc.NamelessBungee.commands.ReportCommand;
+import com.namelessmc.NamelessBungee.commands.SetGroupCommand;
 
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
 
 public class NamelessPlugin extends Plugin {
 
 	private static NamelessPlugin instance;
 
-	private String apiURL = "";
-	private boolean hasSetUrl = false;
+	public static URL baseApiURL;
+	public static boolean https;
 
 	public static final String permission = "namelessmc";
 	public static final String permissionAdmin = "namelessmc.admin";
@@ -28,53 +30,69 @@ public class NamelessPlugin extends Plugin {
 	public void onEnable() {
 		instance = this;
 
-		// Register the API
-		api = new NamelessAPI(this);
-
-		// Init config files.
-		api.getConfigManager().initializeFiles();
-
-		if (hasSetUrl) {
-			registerListeners();
+		try {
+			Config.initialize();
+		} catch (IOException e) {
+			NamelessChat.log(Level.SEVERE, "Unable to load config.");
+			e.printStackTrace();
+			return;
 		}
-		if (getAPI().getConfigManager().getConfig().getBoolean("update-checker")) {
-			checkForUpdate();
+		
+		Configuration config = Config.MAIN.getConfig();
+		String url = config.getString("api-url");
+		if (url.equals("")){
+			NamelessChat.log(Level.SEVERE, "&4No API URL set in the NamelessMC configuration. Nothing will work until you set the correct url.");
+			return; //Prevent registering of commands, listeners, etc.
 		} else {
-			NamelessChat.sendToLog(NamelessMessages.PREFIX_WARNING, "&CIt is recommended to enable update checker.");
+			try {
+				baseApiURL = new URL(url);
+			} catch (MalformedURLException e) {
+				//There is an exception, so the connection was not successful.
+				NamelessChat.log(Level.SEVERE, "&4Invalid API Url/Key. Nothing will work until you set the correct url.");
+				NamelessChat.log(Level.SEVERE, "Error: " + e.getMessage());
+				return; //Prevent registering of commands, listeners, etc.
+			}
+			
+			Exception exception = NamelessAPI.checkWebAPIConnection(baseApiURL);
+			if (exception != null) {
+				//There is an exception, so the connection was not successful.
+				NamelessChat.log(Level.SEVERE, "&4Invalid API Url/Key. Nothing will work until you set the correct url.");
+				NamelessChat.log(Level.SEVERE, "Error: " + exception.getMessage());
+				return; //Prevent registering of commands, listeners, etc.
+			}
 		}
-
+		
+		//Connection is successful, move on with registering listeners and commands.
+		
+		registerCommands();
+		
+		getProxy().getPluginManager().registerListener(this, new PlayerEventListener());
 	}
-
-	public void registerListeners() {
-		if (api.getConfigManager().getCommandsConfig().getBoolean("Commands.Alone.Use")
-				&& api.getConfigManager().getCommandsConfig().getBoolean("Commands.SubCommand.Use")) {
-			NamelessChat.sendToLog(NamelessMessages.PREFIX_WARNING,
-					"&4ERROR REGISTERING COMMANDS! BOUTH IS SET TO TRUE!");
-		} else if (api.getConfigManager().getCommandsConfig().getBoolean("Commands.Alone.Use")) {
-			String register = api.getConfigManager().getCommandsConfig().getString("Commands.Alone.Register");
-			String getUser = api.getConfigManager().getCommandsConfig().getString("Commands.Alone.GetUser");
-			String getNotifications = api.getConfigManager().getCommandsConfig()
-					.getString("Commands.Alone.GetNotifications");
-			String setGroup = api.getConfigManager().getCommandsConfig().getString("Commands.Alone.SetGroup");
-			String report = api.getConfigManager().getCommandsConfig().getString("Commands.Alone.Report");
-			if (api.getConfigManager().getConfig().getBoolean("enable-registration")) {
-				getProxy().getPluginManager().registerCommand(this, new RegisterCommand(this, register));
-			}
-			getProxy().getPluginManager().registerCommand(this, new GetUserCommand(this, getUser));
-			getProxy().getPluginManager().registerCommand(this, new GetNotificationsCommand(this, getNotifications));
-			getProxy().getPluginManager().registerCommand(this, new SetGroupCommand(this, setGroup));
-			if (api.getConfigManager().getConfig().getBoolean("enable-reports")) {
-				getProxy().getPluginManager().registerCommand(this, new ReportCommand(this, report));
-			}
-		} else if (api.getConfigManager().getCommandsConfig().getBoolean("Commands.SubCommand.Use")) {
-			String subCommand = api.getConfigManager().getCommandsConfig().getString("Commands.SubCommand.Main");
-			getProxy().getPluginManager().registerCommand(this, new CommandWithArgs(this, subCommand));
-		} else {
-			NamelessChat.sendToLog(NamelessMessages.PREFIX_WARNING, "&4ERROR REGISTERING COMMANDS!");
+	
+	private void registerCommands() {
+		Configuration commandsConfig = Config.COMMANDS.getConfig();
+		
+		if (commandsConfig.getBoolean("Commands.Alone.Use") && commandsConfig.getBoolean("Commands.SubCommand.Use")) {
+			NamelessChat.log(Level.WARNING, "&4Both individual and subcommands are set to true. Please choose either one. Commands will not work until fixed.");
+			return;
 		}
+		
+		if (commandsConfig.getBoolean("Commands.Alone.Use")) {
+			if (commandsConfig.getBoolean("enable-registration"))
+				getProxy().getPluginManager().registerCommand(this, new RegisterCommand(commandsConfig.getString("Commands.Alone.Register")));
 
-		// Register events
-		getProxy().getPluginManager().registerListener(this, new PlayerEventListener(this));
+			getProxy().getPluginManager().registerCommand(this, new GetUserCommand(commandsConfig.getString("Commands.Alone.GetUser")));
+			
+			getProxy().getPluginManager().registerCommand(this, new GetNotificationsCommand(commandsConfig.getString("Commands.Alone.GetNotifications")));
+			
+			getProxy().getPluginManager().registerCommand(this, new SetGroupCommand(commandsConfig.getString("Commands.Alone.SetGroup")));
+			
+			if (commandsConfig.getBoolean("enable-reports"))
+				getProxy().getPluginManager().registerCommand(this, new ReportCommand(commandsConfig.getString("Commands.Alone.Report")));
+				
+		} else {
+			getProxy().getPluginManager().registerCommand(this, new CommandWithArgs(commandsConfig.getString("Commands.SubCommand.Main")));
+		}
 	}
 	
 	public static NamelessPlugin getInstance() {
