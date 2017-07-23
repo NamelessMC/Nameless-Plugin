65902e6f84100d2374474fb7bb0e3c1bfdc0883a
RobinMC <robinslot@hotmail.nl>
2017-07-23 12:21:29 +0200

    Get notifications command

--- NamelessBungee/src/com/namelessmc/NamelessBungee/commands/GetNotificationsCommand.java
+++ NamelessBungee/src/com/namelessmc/NamelessBungee/commands/GetNotificationsCommand.java
@@ -1,100 +1,79 @@
 package com.namelessmc.NamelessBungee.commands;
 
-import com.namelessmc.namelessplugin.bungeecord.NamelessPlugin;
-import com.namelessmc.namelessplugin.bungeecord.API.Player.NamelessPlayer;
-import com.namelessmc.namelessplugin.bungeecord.API.Player.NamelessPlayerNotifications;
-import com.namelessmc.namelessplugin.bungeecord.API.Utils.NamelessChat;
-import com.namelessmc.namelessplugin.bungeecord.API.Utils.NamelessMessages;
+import com.namelessmc.NamelessAPI.NamelessException;
+import com.namelessmc.NamelessAPI.NamelessPlayer;
+import com.namelessmc.NamelessBungee.NamelessMessages;
+import com.namelessmc.NamelessBungee.NamelessPlugin;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
-
-/*
- *  GetNotifications CMD
- */
 
 public class GetNotificationsCommand extends Command {
 
 	private String commandName;
 
-	public GetNotificationsCommand(String name) {
-		super(name);
-		commandName = name;
+	public GetNotificationsCommand(String commandName) {
+		super(commandName);
+		this.commandName = commandName;
 	}
 
-	/*
-	 * Handle inputted command
-	 */
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		// check if player has permission Permission & ensure who inputted
-		// command is a Player
-		if (sender instanceof ProxiedPlayer) {
-			if (sender.hasPermission(NamelessPlugin.permission + ".notifications")) {
-				ProxiedPlayer player = (ProxiedPlayer) sender;
-				NamelessPlayer namelessPlayer = plugin.getAPI().getPlayer(player.getUniqueId().toString());
-				if (namelessPlayer.exists()) {
-					if (namelessPlayer.isValidated()) {
-						// Try to getNotifications
-						ProxyServer.getInstance().getScheduler().runAsync(plugin, new Runnable() {
-							@Override
-							public void run() {
-								if (args.length > 0) {
-									sender.sendMessage(NamelessChat.convertColors(
-											NamelessChat.getMessage(NamelessMessages.INCORRECT_USAGE_GETNOTIFICATIONS)
-													.replaceAll("%command%", commandName)));
-									return;
-								} else {
-
-									NamelessPlayerNotifications notifications = namelessPlayer.getNotifications();
-									Integer pms = notifications.getPMs();
-									Integer alerts = notifications.getAlerts();
-									String errorMessage = notifications.getErrorMessage();
-									boolean hasError = notifications.hasError();
-
-									String pmMessage = NamelessChat
-											.getMessage(NamelessMessages.PM_NOTIFICATIONS_MESSAGE)
-											.replaceAll("%pms%", pms.toString());
-									String alertMessage = NamelessChat
-											.getMessage(NamelessMessages.ALERTS_NOTIFICATIONS_MESSAGE)
-											.replaceAll("%alerts%", alerts.toString());
-									String noNotifications = NamelessChat.getMessage(NamelessMessages.NO_NOTIFICATIONS);
-
-									if (hasError) {
-										// Error with request
-										sender.sendMessage(NamelessChat.convertColors("&4Error: " + errorMessage));
-									} else if (alerts.equals(0) && pms.equals(0)) {
-										sender.sendMessage(NamelessChat.convertColors(noNotifications));
-									} else if (alerts.equals(0)) {
-										sender.sendMessage(NamelessChat.convertColors(pmMessage));
-									} else if (pms.equals(0)) {
-										sender.sendMessage(NamelessChat.convertColors(alertMessage));
-									} else {
-										sender.sendMessage(NamelessChat.convertColors(alertMessage));
-										sender.sendMessage(NamelessChat.convertColors(pmMessage));
-									}
-								}
-
-							}
-						});
-					} else {
-						sender.sendMessage(
-								NamelessChat.convertColors(NamelessChat.getMessage(NamelessMessages.PLAYER_NOT_VALID)));
-					}
-				} else {
-					sender.sendMessage(
-							NamelessChat.convertColors(NamelessChat.getMessage(NamelessMessages.MUST_REGISTER)));
-				}
-
-			} else {
-				sender.sendMessage(NamelessChat.convertColors(NamelessChat.getMessage(NamelessMessages.NO_PERMISSION)));
-			}
-		} else {
-			// User must be ingame to use register command
-			sender.sendMessage(NamelessChat.convertColors(NamelessChat.getMessage(NamelessMessages.MUST_BE_INGAME)));
+		if (!sender.hasPermission(NamelessPlugin.permission + ".notifications")) {
+			sender.sendMessage(NamelessMessages.NO_PERMISSION.getComponents());
+			return;
 		}
+		
+		if (args.length != 0) {
+			sender.sendMessage(TextComponent.fromLegacyText(
+					NamelessMessages.INCORRECT_USAGE_GETNOTIFICATIONS.getMessage().replace("%command%", commandName)));
+			return;
+		}
+		
+		if (!(sender instanceof ProxiedPlayer)) {
+			sender.sendMessage(NamelessMessages.MUST_BE_INGAME.getComponents());
+			return;
+		}
+		
+		ProxiedPlayer player = (ProxiedPlayer) sender;
+		
+		ProxyServer.getInstance().getScheduler().runAsync(NamelessPlugin.getInstance(), () -> {
+			NamelessPlayer nameless = new NamelessPlayer(player.getUniqueId(), NamelessPlugin.baseApiURL);
+			
+			int messages;
+			int alerts;
+			
+			try {
+				messages = nameless.getMessageCount();
+				alerts = nameless.getAlertCount();
+			} catch (NamelessException e) {
+				player.sendMessage(new ComponentBuilder("An error occured: " + e.getMessage()).color(ChatColor.RED).create());
+				return;
+			}
+			
+			BaseComponent[] pmMessage = TextComponent.fromLegacyText(
+					NamelessMessages.PM_NOTIFICATIONS_MESSAGE.getMessage().replace("%pms%", "" + messages));
+			BaseComponent[] alertMessage = TextComponent.fromLegacyText(
+					NamelessMessages.ALERTS_NOTIFICATIONS_MESSAGE.getMessage().replace("%alerts%", "" + alerts));
+			BaseComponent[] noNotifications = NamelessMessages.NO_NOTIFICATIONS.getComponents();
+
+			if (alerts == 0 && messages == 0) {
+				sender.sendMessage(noNotifications);
+			} else if (alerts == 0) {
+				sender.sendMessage(pmMessage);
+			} else if (messages == 0) {
+				sender.sendMessage(alertMessage);
+			} else {
+				sender.sendMessage(alertMessage);
+				sender.sendMessage(pmMessage);
+			}
+		});
 	}
 
 }
