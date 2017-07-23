bb53953f3d5759fe2856b0006ed807a0ba73b45e
RobinMC <robinslot@hotmail.nl>
2017-07-23 12:06:35 +0200

    Get user command

--- NamelessBungee/src/com/namelessmc/NamelessBungee/commands/GetUserCommand.java
+++ NamelessBungee/src/com/namelessmc/NamelessBungee/commands/GetUserCommand.java
@@ -1,106 +1,105 @@
 package com.namelessmc.NamelessBungee.commands;
 
-import com.namelessmc.namelessplugin.bungeecord.NamelessPlugin;
-import com.namelessmc.namelessplugin.bungeecord.API.NamelessAPI;
-import com.namelessmc.namelessplugin.bungeecord.API.Player.NamelessPlayer;
-import com.namelessmc.namelessplugin.bungeecord.API.Utils.NamelessChat;
-import com.namelessmc.namelessplugin.bungeecord.API.Utils.NamelessMessages;
+import java.util.UUID;
 
+import com.namelessmc.NamelessAPI.NamelessPlayer;
+import com.namelessmc.NamelessBungee.NamelessMessages;
+import com.namelessmc.NamelessBungee.NamelessPlugin;
+import com.namelessmc.NamelessBungee.util.UUIDFetcher;
+
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
-
-/*
- *  GetUserCommand CMD
- */
 
 public class GetUserCommand extends Command {
 
 	private String commandName;
 
-	public GetUserCommand(String name) {
-		super(name);
-		commandName = name;
+	public GetUserCommand(String commandName) {
+		super(commandName);
+		this.commandName = commandName;
 	}
 
-	/*
-	 * Handle inputted command
-	 */
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		if (sender.hasPermission(NamelessPlugin.permissionAdmin + ".getUser")) {
-			// Try to get the user
-			ProxyServer.getInstance().getScheduler().runAsync(plugin, new Runnable() {
-				@Override
-				public void run() {
-					// Ensure username or uuid set.
-					if (args.length < 1 || args.length > 1) {
-						sender.sendMessage(NamelessChat
-								.convertColors(NamelessChat.getMessage(NamelessMessages.INCORRECT_USAGE_GETUSER)
-										.replaceAll("%command%", commandName)));
-						return;
-					} else {
+		if (!sender.hasPermission(NamelessPlugin.permissionAdmin + ".getUser")) {
+			sender.sendMessage(NamelessMessages.NO_PERMISSION.getComponents());
+			return;
+		}
+		
+		if (args.length != 1) {
+			sender.sendMessage(TextComponent.fromLegacyText(
+					NamelessMessages.INCORRECT_USAGE_REPORT.getMessage().replace("%command%", commandName)));
+			return;
+		}
+		
+		ProxyServer.getInstance().getScheduler().runAsync(NamelessPlugin.getInstance(), new Runnable() {
+			@Override
+			public void run() {
+				final String targetName = args[0];
+				final UUID targetUuid;
+				
+				try {
+					targetUuid = UUIDFetcher.getUUID(targetName);
+				} catch (IllegalArgumentException e) {
+					sender.sendMessage(new ComponentBuilder("This player could not be found").color(ChatColor.RED).create()); // TODO Use messages.yml
+					return;
+				}
+				
+				NamelessPlayer target = new NamelessPlayer(targetUuid, NamelessPlugin.baseApiURL);
 
-						NamelessAPI api = plugin.getAPI();
-						NamelessPlayer namelessPlayer = api.getPlayer(args[0]);
+				BaseComponent[] separator = new ComponentBuilder("--------------------------------").color(ChatColor.DARK_AQUA).italic(true).create();
+				
+				sender.sendMessage(separator);
+				
+				sender.sendMessage(TextComponent.fromLegacyText(
+						NamelessMessages.GETUSER_USERNAME.getMessage().replace("%username%", target.getUsername())));
+				
+				sender.sendMessage(TextComponent.fromLegacyText(
+						NamelessMessages.GETUSER_DISPLAYNAME.getMessage().replace("%displayname%", target.getDisplayName())));
+				
+				sender.sendMessage(TextComponent.fromLegacyText(
+						NamelessMessages.GETUSER_UUID.getMessage().replace("%uuid%", targetUuid.toString())));
+				
+				sender.sendMessage(TextComponent.fromLegacyText(
+						NamelessMessages.GETUSER_GROUP_ID.getMessage().replace("%groupid%", "" + target.getGroupID())));
+		
+				sender.sendMessage(TextComponent.fromLegacyText(
+						NamelessMessages.GETUSER_REGISTERED.getMessage().replace("%registereddate%", target.getRegisteredDate().toString())));
+				
+				sender.sendMessage(TextComponent.fromLegacyText(
+						NamelessMessages.GETUSER_REPUTATION.getMessage().replace("%reputation%", "" + target.getReputations())));
 
-						if (namelessPlayer.hasError()) {
-							// Error with request
-							sender.sendMessage(
-									NamelessChat.convertColors("&4Error: &c" + namelessPlayer.getErrorMessage()));
-						} else {
-
-							// Display get user.
-							String line = "&3&m--------------------------------";
-
-							sender.sendMessage(NamelessChat.convertColors(line));
-							sender.sendMessage(NamelessChat
-									.convertColors(NamelessChat.getMessage(NamelessMessages.GETUSER_USERNAME)
-											.replaceAll("%username%", namelessPlayer.getUserName())));
-							sender.sendMessage(NamelessChat
-									.convertColors(NamelessChat.getMessage(NamelessMessages.GETUSER_DISPLAYNAME)
-											.replaceAll("%displayname%", namelessPlayer.getDisplayName())));
-							sender.sendMessage(
-									NamelessChat.convertColors(NamelessChat.getMessage(NamelessMessages.GETUSER_UUID)
-											.replaceAll("%uuid%", namelessPlayer.getUUID())));
-							sender.sendMessage(NamelessChat
-									.convertColors(NamelessChat.getMessage(NamelessMessages.GETUSER_GROUP_ID)
-											.replaceAll("%groupid%", namelessPlayer.getGroupID().toString())));
-							sender.sendMessage(NamelessChat.convertColors(
-									NamelessChat.getMessage(NamelessMessages.GETUSER_REGISTERED).replaceAll(
-											"%registereddate%", namelessPlayer.getRegisteredDate().toString())));
-							sender.sendMessage(NamelessChat
-									.convertColors(NamelessChat.getMessage(NamelessMessages.GETUSER_REPUTATION)
-											.replaceAll("%reputation%", namelessPlayer.getReputations().toString())));
-
-							// check if validated
-							if (namelessPlayer.isValidated()) {
-								sender.sendMessage(NamelessChat.convertColors(
-										NamelessChat.getMessage(NamelessMessages.GETUSER_VALIDATED) + "&a: "
+				/*if (namelessPlayer.isValidated()) { TODO Check if verified
+						sender.sendMessage(NamelessChat.convertColors(
+								NamelessChat.getMessage(NamelessMessages.GETUSER_VALIDATED) + "&a: "
 												+ NamelessChat.getMessage(NamelessMessages.GETUSER_VALIDATED_YES)));
 							} else {
 								sender.sendMessage(NamelessChat.convertColors(
 										NamelessChat.getMessage(NamelessMessages.GETUSER_VALIDATED) + "&c: "
 												+ NamelessChat.getMessage(NamelessMessages.GETUSER_VALIDATED_NO)));
-							}
-							// check if banned
-							if (namelessPlayer.isBanned()) {
-								sender.sendMessage(NamelessChat
-										.convertColors(NamelessChat.getMessage(NamelessMessages.GETUSER_BANNED) + "&c: "
-												+ NamelessChat.getMessage(NamelessMessages.GETUSER_BANNED_YES)));
-							} else {
-								sender.sendMessage(NamelessChat
-										.convertColors(NamelessChat.getMessage(NamelessMessages.GETUSER_BANNED) + "&a: "
-												+ NamelessChat.getMessage(NamelessMessages.GETUSER_BANNED_NO)));
-							}
-							sender.sendMessage(NamelessChat.convertColors(line));
-						}
-					}
+							}*/
+				
+				if (target.isBanned()) {
+					sender.sendMessage(TextComponent.fromLegacyText(
+							NamelessMessages.GETUSER_BANNED.getMessage() +
+							"&c: " +
+							NamelessMessages.GETUSER_BANNED_YES.getMessage()));
+				} else {
+					sender.sendMessage(TextComponent.fromLegacyText(
+							NamelessMessages.GETUSER_BANNED.getMessage() +
+							"&a: " +
+							NamelessMessages.GETUSER_BANNED_NO.getMessage()));
 				}
-			});
-		} else {
-			sender.sendMessage(NamelessChat.convertColors(NamelessChat.getMessage(NamelessMessages.NO_PERMISSION)));
-		}
+				
+				sender.sendMessage(separator);
+			}
+		});
+
 	}
 
 }
