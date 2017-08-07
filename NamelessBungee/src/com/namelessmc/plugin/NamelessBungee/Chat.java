ec4d5adacb3b0556b349e6bd09b0fa71bae5a740
RobinMC <robinslot@hotmail.nl>
2017-08-07 22:24:51 +0200

    Remove unused method

--- NamelessBungee/src/com/namelessmc/plugin/NamelessBungee/Chat.java
+++ NamelessBungee/src/com/namelessmc/plugin/NamelessBungee/Chat.java
@@ -3,20 +3,8 @@ package com.namelessmc.plugin.NamelessBungee;
 import java.util.logging.Level;
 
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 
 public class Chat {
-
-	public static TextComponent sendClickableMessage(String message, ClickEvent.Action click, String value,
-			HoverEvent.Action hover, String hoverText) {
-		TextComponent msg = new TextComponent(ChatColor.translateAlternateColorCodes('&', message));
-		msg.setClickEvent(new ClickEvent(click, value));
-		msg.setHoverEvent(new HoverEvent(hover, new ComponentBuilder(convertColorsString(hoverText)).create()));
-		return msg;
-	}
 
 	public static String convertColorsString(String message) {
 		return ChatColor.translateAlternateColorCodes('&', message);
