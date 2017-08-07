2577c1179d4ad837484dcb442db9f98c993e24e8
RobinMC <robinslot@hotmail.nl>
2017-08-07 16:46:01 +0200

    Move log method to out of Chat class

--- NamelessSpigot/src/com/namelessmc/plugin/NamelessSpigot/Chat.java
+++ NamelessSpigot/src/com/namelessmc/plugin/NamelessSpigot/Chat.java
@@ -1,17 +1,11 @@
 package com.namelessmc.plugin.NamelessSpigot;
 
-import java.util.logging.Level;
-
 import org.bukkit.ChatColor;
 
 public class Chat {
 
 	public static String convertColors(String message) {
 		return ChatColor.translateAlternateColorCodes('&', message);
-	}
-	
-	public static void log(Level level, String message) {
-		NamelessPlugin.getInstance().getLogger().log(level, convertColors(message));
 	}
 
 }
