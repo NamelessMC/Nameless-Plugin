bba38a3f48cc58e868a1798577f6ff76be4fa79c
IsS127 <saker.web1@gmail.com>
2017-10-30 23:20:47 +0100

    Permissions [Sponge]

---
+++ NamelessSponge/6.0.0/src/com/namelessmc/plugin/NamelessSponge/permissions/Permissions.java
@@ -0,0 +1,9 @@
+package com.namelessmc.plugin.NamelessSponge.permissions;
+
+import org.spongepowered.api.entity.living.player.User;
+
+public interface Permissions {
+	
+	public String getGroup(User user);
+
+}
