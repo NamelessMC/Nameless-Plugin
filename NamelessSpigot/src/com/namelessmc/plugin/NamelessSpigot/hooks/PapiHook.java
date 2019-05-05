ff56cfb1b963b6dceba795235df4e7722bfa60be
Robin <robinslot@hotmail.nl>
2019-05-05 21:10:13 +0200

    Update PlaceholderAPI hook
    The old one will be removed soon

--- NamelessSpigot/src/com/namelessmc/plugin/NamelessSpigot/hooks/PapiHook.java
+++ NamelessSpigot/src/com/namelessmc/plugin/NamelessSpigot/hooks/PapiHook.java
@@ -4,28 +4,46 @@ import org.bukkit.entity.Player;
 
 import com.namelessmc.plugin.NamelessSpigot.NamelessPlugin;
 
-import me.clip.placeholderapi.external.EZPlaceholderHook;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import xyz.derkades.derkutils.caching.Cache;
 
-public class PapiHook extends EZPlaceholderHook {
+public class PapiHook extends PlaceholderExpansion {
 
-	public PapiHook() {
-		super(NamelessPlugin.getInstance(), "nameless");
+	@Override
+	public String onPlaceholderRequest(Player player, String identifier) {	
+		if (identifier.equals("notifications") && player != null) {
+			Object cache = Cache.getCachedObject("nlmc-not" + player.getName());
+			if (cache == null)
+				return "?";
+			else
+				return String.valueOf((int) cache);
+		}
+		return null;
 	}
 
 	@Override
-	public String onPlaceholderRequest(Player player, String identifier) {
-		if (!identifier.equals("notifications")) {
-			return null;
-		}
-		
-		Object cache = Cache.getCachedObject("nlmc-not" + player.getName());
-		
-		if (cache == null) {
-			return "0";
-		} else {
-			return String.valueOf((int) cache);
-		}
+	public String getAuthor() {
+		return String.join(", ", NamelessPlugin.getInstance().getDescription().getAuthors());
 	}
+
+	@Override
+	public String getIdentifier() {
+		return "nameless";
+	}
+
+	@Override
+	public String getVersion() {
+		return NamelessPlugin.getInstance().getDescription().getVersion();
+	}
+	
+    @Override
+    public boolean persist(){
+        return true;
+    }
+    
+    @Override
+    public boolean canRegister(){
+        return true;
+    }
 
 }
