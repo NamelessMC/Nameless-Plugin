4297f8f1bcf61be29c981a81144a17447e32d7b0
Robin <robinslot@hotmail.nl>
2019-08-06 19:17:04 +0200

    Remove old permissions

--- NamelessSpigot/src/com/namelessmc/plugin/NamelessSpigot/Permission.java
+++ NamelessSpigot/src/com/namelessmc/plugin/NamelessSpigot/Permission.java
@@ -5,47 +5,39 @@ import org.bukkit.permissions.PermissionDefault;
 
 public enum Permission {
 
-	//COMMAND_REGISTER("namelessmc.register"),
-	//COMMAND_GETNOTIFICATIONS("namelessmc.notifications"),
-	//COMMAND_REPORT("namelessmc.report"),
-	//COMMAND_ADMIN_SETGROUP("namelessmc.admin.setgroup"),
-	//COMMAND_ADMIN_GETUSER("namelessmc.admin.getuser"),
-	//COMMAND_VALIDATE("namelessmc.validate"),
-	//ADMIN_UPDATENOTIFY("namelessmc.admin.updatenotify");
-	
 	COMMAND_GET_NOTIFICATIONS("namelessmc.command.getnotifications"),
 	COMMAND_REGISTER("namelessmc.command.register"),
 	COMMAND_REPORT("namelessmc.command.report"),
 	COMMAND_SET_GROUP("namelessmc.command.setgroup"),
 	COMMAND_USER_INFO("namelessmc.command.userinfo"),
 	COMMAND_VALIDATE("namelessmc.command.validate"),
-	
+
 	COMMAND_NAMELESS("namelessmc.command.nameless"),
-	
+
 	;
-	
+
 	private org.bukkit.permissions.Permission permission;
 	private String permissionString;
-	
-	Permission(String permissionString){
-		permission = new org.bukkit.permissions.Permission(permissionString);
+
+	Permission(final String permissionString){
+		this.permission = new org.bukkit.permissions.Permission(permissionString);
 		this.permissionString = permissionString;
 	}
-	
+
 	@Override
 	public String toString() {
-		return permissionString;
+		return this.permissionString;
 	}
-	
+
 	public org.bukkit.permissions.Permission asPermission() {
-		return permission;
+		return this.permission;
 	}
-	
-	public boolean hasPermission(CommandSender sender) {
-		return sender.hasPermission(asPermission());
+
+	public boolean hasPermission(final CommandSender sender) {
+		return sender.hasPermission(this.asPermission());
 	}
-	
-	public static org.bukkit.permissions.Permission toGroupSyncPermission(String permission) {
+
+	public static org.bukkit.permissions.Permission toGroupSyncPermission(final String permission) {
 		return new org.bukkit.permissions.Permission(permission, PermissionDefault.FALSE);
 	}
 
