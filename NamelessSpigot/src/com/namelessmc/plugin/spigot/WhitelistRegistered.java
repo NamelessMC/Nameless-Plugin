34db2373c6a9f08a3b541dd37a665902aff4cbac
Derkades <robinslot@hotmail.nl>
2020-10-10 19:48:58 +0200

    Update auto whitelister

--- NamelessSpigot/src/com/namelessmc/plugin/spigot/WhitelistRegistered.java
+++ NamelessSpigot/src/com/namelessmc/plugin/spigot/WhitelistRegistered.java
@@ -1,14 +1,18 @@
 package com.namelessmc.plugin.spigot;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
 import com.namelessmc.java_api.NamelessException;
+import com.namelessmc.java_api.NamelessUser;
+import com.namelessmc.java_api.UserFilter;
 
 public class WhitelistRegistered implements Runnable {
 
@@ -23,22 +27,39 @@ public class WhitelistRegistered implements Runnable {
 
 	@Override
 	public void run() {
-		final boolean hideInactive = Config.MAIN.getConfig().getBoolean("auto-whitelist-registered.exclude-inactive");
-		final boolean hideBanned = Config.MAIN.getConfig().getBoolean("auto-whitelist-registered.exclude-banned");
+		final boolean verified = Config.MAIN.getConfig().getBoolean("auto-whitelist-registered.verified");
 		final boolean log = Config.MAIN.getConfig().getBoolean("auto-whitelist-registered.log");
-
 		final Logger logger = NamelessPlugin.getInstance().getLogger();
 
 		Bukkit.getScheduler().runTaskAsynchronously(NamelessPlugin.getInstance(), () -> {
-			final Set<UUID> uuids;
+			UserFilter<?>[] filters;
+			if (verified) {
+				filters = new UserFilter<?>[] {UserFilter.VERIFIED, UserFilter.UNBANNED};
+			} else {
+				filters = new UserFilter<?>[] {UserFilter.UNBANNED};
+			}
+			
+			List<NamelessUser> users;
 			try {
-				uuids = NamelessPlugin.getInstance().api.getRegisteredUsers(hideInactive, hideBanned).keySet();
+				users = NamelessPlugin.getInstance().api.getRegisteredUsers(filters);
 			} catch (final NamelessException e) {
 				logger.warning(
 						"An error occured while getting a list of registered users from the website for the auto-whitelist-registered feature.");
 				e.printStackTrace();
 				return;
 			}
+			
+			final Set<UUID> uuids = users.stream()
+					.map(user -> {
+						try {
+							return user.getUniqueId();
+						} catch (final NamelessException e) {
+							return null;
+						}
+					})
+					.filter(opt -> opt != null && opt.isPresent())
+					.map(opt -> opt.get())
+					.collect(Collectors.toSet());
 			
 			// Use Set for O(1) contains()
 			final Set<String> excludes = new HashSet<>();
