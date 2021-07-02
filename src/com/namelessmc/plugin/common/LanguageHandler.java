aa6e4a5bf27970e1b679367bb18db9b2d3a919b1
Robin <robinslot@hotmail.nl>
2021-07-02 10:45:10 +0200

    Release 2.0.2

--- src/com/namelessmc/plugin/common/LanguageHandler.java
+++ src/com/namelessmc/plugin/common/LanguageHandler.java
@@ -97,7 +97,7 @@ public class LanguageHandler {
 	/**
 	 * Language version. Increment by one when adding, removing, or changing strings.
 	 */
-	private static final int VERSION = 15;
+	private static final int VERSION = 16;
 
 	private static final String[] LANGUAGES_LIST = {
 			"cs_CZ",
