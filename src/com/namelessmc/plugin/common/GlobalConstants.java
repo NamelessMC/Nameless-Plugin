8fe535cf76c9d6a1a375ae6a9b10ec5d2cbccc0c
Robin <robin@derkad.es>
2021-11-01 11:42:28 +0100

    Second set of supported but deprecated website versions

--- src/com/namelessmc/plugin/common/GlobalConstants.java
+++ src/com/namelessmc/plugin/common/GlobalConstants.java
@@ -8,8 +8,11 @@ import java.util.Set;
 public class GlobalConstants {
 
 	public static final Set<NamelessVersion> SUPPORTED_WEBSITE_VERSIONS = EnumSet.of(
-			NamelessVersion.V2_0_0_PR_12,
 			NamelessVersion.V2_0_0_PR_13
 	);
+
+	public static final Set<NamelessVersion> DEPRECATED_WEBSITE_VERSIONS = EnumSet.of(
+			NamelessVersion.V2_0_0_PR_12
+	)
 
 }
