e1df713cc70746e2bc62acc7ecac17f063eb0cf8
Robin <robin+git@rkslot.nl>
2022-04-04 17:57:00 +0200

    Delete unused class

--- src/com/namelessmc/plugin/velocity/JulToSl4j.java
+++
@@ -1,9 +0,0 @@
-package com.namelessmc.plugin.velocity;
-
-import java.util.logging.Logger;
-
-public class JulToSl4j extends Logger {
-	protected JulToSl4j(String name, String resourceBundleName) {
-		super(name, resourceBundleName);
-	}
-}
