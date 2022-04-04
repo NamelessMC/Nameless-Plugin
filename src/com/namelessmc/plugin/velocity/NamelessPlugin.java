209801506db49f664d1ac67f62b08d77ec9f230b
Robin <robin+git@rkslot.nl>
2022-04-04 18:00:30 +0200

    Remove more unused code

--- src/com/namelessmc/plugin/velocity/NamelessPlugin.java
+++ src/com/namelessmc/plugin/velocity/NamelessPlugin.java
@@ -16,10 +16,8 @@ import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
-import net.md_5.bungee.config.Configuration;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
-import org.yaml.snakeyaml.Yaml;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -46,9 +44,6 @@ public class NamelessPlugin implements CommonObjectsProvider {
 
 	private AbstractLogger commonLogger;
 	@Override public AbstractLogger getCommonLogger() { return this.commonLogger; }
-
-	private final @NotNull Yaml yaml = new Yaml();
-	private Configuration mainConfig;
 
 	private final ArrayList<String> registeredCommands = new ArrayList<>();
 
