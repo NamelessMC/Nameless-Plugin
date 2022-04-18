74a97d7095a707ea2c066a8ebfb8391cdbe82418
Robin <robinslot@hotmail.nl>
2022-04-18 23:22:08 +0200

    experimenting with kyori event, will continue tomorrow

--- src/main/com/namelessmc/plugin/common/NamelessPlugin.java
+++ src/main/com/namelessmc/plugin/common/NamelessPlugin.java
@@ -1,7 +1,9 @@
 package com.namelessmc.plugin.common;
 
 import com.namelessmc.plugin.common.command.AbstractScheduler;
+import com.namelessmc.plugin.common.event.AbstractEvent;
 import com.namelessmc.plugin.common.logger.AbstractLogger;
+import net.kyori.event.EventBus;
 import net.md_5.bungee.config.Configuration;
 import org.bstats.MetricsBase;
 import org.bstats.charts.SimplePie;
@@ -21,6 +23,7 @@ public class NamelessPlugin {
 	private final @NotNull ApiProvider api;
 	private final @NotNull LanguageHandler language;
 	private final @NotNull DateFormatter dateFormatter;
+	private final @NotNull EventBus<AbstractEvent> eventBus;
 
 	private final @NotNull List<Reloadable> reloadables = new ArrayList<>();
 	public final @NotNull Map<UUID, Long> playerLoginTime = new HashMap<>();
@@ -54,6 +57,8 @@ public class NamelessPlugin {
 				playerLoginTime.put(player.getUniqueId(), System.currentTimeMillis());
 			}
 		});
+
+		this.eventBus = EventBus.create(AbstractEvent.class);
 	}
 
 	public ConfigurationHandler config() {
---
+++ src/main/com/namelessmc/plugin/common/event/JoinEvent.java
@@ -0,0 +1,4 @@
+package com.namelessmc.plugin.common.event;
+
+public class JoinEvent extends AbstractEvent {
+}
