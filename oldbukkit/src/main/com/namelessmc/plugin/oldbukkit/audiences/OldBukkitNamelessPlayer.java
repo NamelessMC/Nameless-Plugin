8d07626c5f3b1170a30f705a5a6cfe83590dad7f
Robin Slot <robin+github@rkslot.nl>
2024-07-31 12:14:19 +0200

    Extend BukkitNamelessPlayer instead of NamelessPlayer

--- oldbukkit/src/main/com/namelessmc/plugin/oldbukkit/audiences/OldBukkitNamelessPlayer.java
+++ oldbukkit/src/main/com/namelessmc/plugin/oldbukkit/audiences/OldBukkitNamelessPlayer.java
@@ -1,22 +1,15 @@
 package com.namelessmc.plugin.oldbukkit.audiences;
 
-import com.namelessmc.plugin.common.ConfigurationHandler;
-import com.namelessmc.plugin.common.Permission;
-import com.namelessmc.plugin.common.audiences.NamelessPlayer;
 import org.bukkit.entity.Player;
 
-public class OldBukkitNamelessPlayer extends NamelessPlayer {
+import com.namelessmc.plugin.bukkit.audiences.BukkitNamelessPlayer;
+import com.namelessmc.plugin.common.ConfigurationHandler;
 
-	private final Player player;
+public class OldBukkitNamelessPlayer extends BukkitNamelessPlayer {
 
 	public OldBukkitNamelessPlayer(final ConfigurationHandler config,
 								   final Player player) {
-		super(config, new LegacyCommandSenderAudience(player), player.getUniqueId(), player.getName());
-		this.player = player;
+		super(config, new LegacyCommandSenderAudience(player), player);
 	}
 
-	@Override
-	public boolean hasPermission(Permission permission) {
-		return this.player.hasPermission(permission.toString());
-	}
 }
