14684c4cb61a9dbc87ae70761467cda2c1d4378d
Robin Slot <robin@rslot.nl>
2025-11-03 21:21:47 +0100

    implement permission check

--- sponge8/src/main/com/namelessmc/plugin/sponge8/audiences/SpongeNamelessPlayer.java
+++ sponge8/src/main/com/namelessmc/plugin/sponge8/audiences/SpongeNamelessPlayer.java
@@ -1,10 +1,17 @@
 package com.namelessmc.plugin.sponge8.audiences;
 
+import java.util.NoSuchElementException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.User;
+
 import com.namelessmc.plugin.common.ConfigurationHandler;
 import com.namelessmc.plugin.common.Permission;
 import com.namelessmc.plugin.common.audiences.NamelessPlayer;
-import org.apache.commons.lang3.NotImplementedException;
-import org.spongepowered.api.entity.living.player.Player;
 
 public class SpongeNamelessPlayer extends NamelessPlayer {
 
@@ -17,8 +24,14 @@ public class SpongeNamelessPlayer extends NamelessPlayer {
 	}
 
 	@Override
-	public boolean hasPermission(Permission permission) {
-		throw new NotImplementedException("How to check for permissions in sponge?");
+	public boolean hasPermission(final Permission permission) {
+		try {
+			final User user = Sponge.server().userManager().load(this.player.uniqueId()).get(100, TimeUnit.MILLISECONDS).orElseThrow();
+			return user.hasPermission(permission.toString());
+		} catch (InterruptedException | ExecutionException | TimeoutException | NoSuchElementException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 }
